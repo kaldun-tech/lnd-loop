bfbc180e4bb22eb0437655369a79eda76c11bb71
Boris Nagaev <bnagaev@gmail.com>
2026-04-15 00:28:59 -0500

    test: add route hint assertion helper

---
+++ test/route_hints.go
@@ -0,0 +1,49 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/lightningnetwork/lnd/zpay32"
+	"github.com/stretchr/testify/require"
+)
+
+// RequireRouteHintsEqual asserts that two route hint sets are identical.
+func RequireRouteHintsEqual(t testing.TB, expected,
+	actual [][]zpay32.HopHint) {
+
+	t.Helper()
+
+	require.Len(t, actual, len(expected))
+
+	for i := range expected {
+		require.Len(t, actual[i], len(expected[i]))
+
+		for j := range expected[i] {
+			expectedHint := expected[i][j]
+			actualHint := actual[i][j]
+
+			require.Equal(
+				t,
+				expectedHint.NodeID.SerializeCompressed(),
+				actualHint.NodeID.SerializeCompressed(),
+			)
+			require.Equal(
+				t, expectedHint.ChannelID, actualHint.ChannelID,
+			)
+			require.Equal(
+				t, expectedHint.FeeBaseMSat,
+				actualHint.FeeBaseMSat,
+			)
+			require.Equal(
+				t,
+				expectedHint.FeeProportionalMillionths,
+				actualHint.FeeProportionalMillionths,
+			)
+			require.Equal(
+				t,
+				expectedHint.CLTVExpiryDelta,
+				actualHint.CLTVExpiryDelta,
+			)
+		}
+	}
+}
